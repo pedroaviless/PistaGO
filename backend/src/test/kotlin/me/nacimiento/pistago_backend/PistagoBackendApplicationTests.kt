ceda4bf7fad4b931f7814043530b2d30c91ef1f9
pedroaviless <al.pedro.aviles.aguilera@iesportada.org>
2026-03-04 18:42:01 +0100

    feat: inicializar proyecto Spring Boot con conexión PostgreSQL

---
+++ backend/src/test/kotlin/me/nacimiento/pistago_backend/PistagoBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.nacimiento.pistago_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PistagoBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
